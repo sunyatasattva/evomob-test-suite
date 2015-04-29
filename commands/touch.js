77ad249316dcaf0e482af5245b6d7f38899446a6
Veren <sunyatasattva@gmail.com>
2015-04-29 23:21:21 +0200

    Adding touch custom command. Allows to emulate touch events.

---
+++ commands/touch.js
@@ -0,0 +1,20 @@
+exports.command = function(selector, type, callback) {
+    var client = this;
+
+    client.execute(function(sel, t) {
+        var event = new MouseEvent(t || 'click', {
+            'view': window,
+            'bubbles': true,
+            'cancelable': true
+        });
+        var el = document.querySelector(sel);
+        el.dispatchEvent(event);
+        return true;
+    }, [selector, type], function(result) {
+        if (typeof callback === 'function') {
+            callback.call(client, result);
+        }
+    });
+
+    return this;
+};
