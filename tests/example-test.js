bb5569ef6a9499c3a404e063ba42ee568669a3a3
Veren <sunyatasattva@gmail.com>
2015-04-30 18:01:56 +0200

    Refactored example-test module to use page objects.

--- tests/example-test.js
+++ tests/example-test.js
@@ -1,16 +1,46 @@
+
 module.exports = {
-  "Search function": function(client) {
-    client
-      .url("http://shop.wki.it")
-      .waitForElementVisible("#aspnetForm", 5000)
-      .click(".wki-icon-search")
-      .assert.visible("#ctl00_BarraRicerca1_TextBoxInizioRicerca")
-      .setValue("#ctl00_BarraRicerca1_TextBoxInizioRicerca", "test")
-      .keys(client.Keys.ENTER)
-      .waitForElementVisible(".x-searchResults", 5000)
-      .assert.urlContains("risultatoricerca")
-      .verify.containsText(".search-keyword", "test")
-      .end()
-  }
+    before: function(client){
+        thisPage = client.page.exampleLayout();
+    },
+    
+    "It gets Remixed correctly": function(client){
+        client
+            .page.exampleLayout().visitPage()
+            .page.exampleLayout().expectedElementsPresent()
+            .verify.attributeContains(thisPage.selectors.slider.links, "href", "http://shop.wki.it/")
+            .verify.elementPresent(thisPage.selectors.slider.images)
+            .verify.elementsCountAtLeast(thisPage.selectors.mainMenuNavElements, 7)
+        .end();
+    },
+    
+    "Search function": function(client){
+        var query = "test";
+        
+        client
+            .page.exampleLayout().visitPage()
+            .page.exampleLayout().searchFor(query)
+            .waitForElementVisible(".x-searchResults", 11000)
+            .assert.urlContains("risultatoricerca")
+            .verify.containsText(".search-keyword", query)
+        .end();
+    },
+    "Search input shows correctly": function(client){
+        client
+            .page.exampleLayout().visitPage()
+            .verify.hidden(thisPage.selectors.search.searchInput, "searchInput should initially be hidden")
+            .page.exampleLayout().openSearch()
+            .verify.visible(thisPage.selectors.search.searchInput, "searchInput should now be visible")
+        .end();
+    },
+    "Slider correctly moves": function(client) {
+        client
+            .page.exampleLayout().visitPage()
+            .verify.hidden( thisPage.getSlideCss(3) )
+            .page.exampleLayout().moveToSlide(3)
+            .pause(1000)
+            .verify.visible( thisPage.getSlideCss(3) )
+        .end();
+    }
 };
 
