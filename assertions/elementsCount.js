6ecc59b944e2bcfeef22d2a1b377ca12c7361298
Veren <sunyatasattva@gmail.com>
2015-04-30 17:57:32 +0200

    Added elementCount assertion.
    It allows to make sure that the number of a given element in DOM is exactly
    as expected.

---
+++ assertions/elementsCount.js
@@ -0,0 +1,34 @@
+var util = require('util');
+exports.assertion = function(selector, expected, msg) {
+
+    var MSG = 'Testing if the count for the element <%s> is %s. ';
+
+    this.message = msg || util.format(MSG, selector, expected);
+
+    this.expected = function() {
+        return expected;
+    };
+
+    this.pass = function(value) {
+        return value === expected;
+    };
+
+    this.failure = function(result) {
+        var failed = result === false || result && result.status === -1;
+        
+        if (failed) {
+            this.message = msg || util.format(MSG, selector, expected);
+        }
+        
+        return failed;
+    };
+
+    this.value = function(result) {
+        return result.value.length;
+    };
+
+    this.command = function(callback) {
+        return this.api.elements('css selector', selector, callback);
+    };
+
+};
